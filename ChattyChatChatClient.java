ce310e8d0646d36e881bc1e625f034bd7891198d
alexanderwchen <calex34567@gmail.com>
2020-04-09 21:14:54 -0400

    Initial Commit

---
+++ ChattyChatChatClient.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ChattyChatChatClient{
+
+    public static void main(String[] args){
+        System.out.println("Start ChattyChatChatClient");
+        System.out.println("End ChattyChatChatClient");
+    }
+
+
+
+}
---
+++ ChattyChatChatServer.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+public class ChattyChatChatServer{
+
+    public static void main(String[] args){
+        System.out.println("Start ChattyChatChatServer");
+        System.out.println("End ChattyChatChatServer");
+    }
+
+
+
+}
