0d3d0b271db5e1d26eec2e4ae149fe302e515d96
alexanderwchen <calex34567@gmail.com>
2020-04-10 20:40:45 -0400

    Add public message capability

--- ChattyChatChatServer.java
+++ ChattyChatChatServer.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 
 public class ChattyChatChatServer{
 
+    public static ArrayList<ClientRunnable> clientRunnables;
+
     public static void main(String[] args){
         try {
             System.out.println("Start ChattyChatChatServer");
@@ -20,14 +22,16 @@ public class ChattyChatChatServer{
             Socket socket = null;
             boolean runServer = true;
             int clientNumber = 0;
-            ArrayList<ClientRunnable> clientRunnables = new ArrayList<ClientRunnable>();
+            clientRunnables = new ArrayList<ClientRunnable>();
 
             try{
                 listener = new ServerSocket( port );
 
                 while(runServer){
                     try {
+                        socket = null;
                         socket = listener.accept();
+                        System.out.println("New Connection Made: " + socket.toString());
                         ClientRunnable tempClientRunnable = new ClientRunnable(socket,clientNumber);
                         clientRunnables.add(tempClientRunnable);
                         new Thread(tempClientRunnable).start();
@@ -35,7 +39,6 @@ public class ChattyChatChatServer{
                     } catch (IOException  e) {
                         System.out.println("Error connecting to client " + clientNumber++);
                     }
-                    runServer = false;
                 }
                 listener.close();
             }
@@ -56,18 +59,26 @@ public class ChattyChatChatServer{
         private String username;
         private int clientNumber;
         private boolean isRunning;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        public String getUsername(){return username;};
+        public int getClientNumber(){return clientNumber;};
+        public boolean getIsRunning(){return isRunning;};
+
+
 
         public ClientRunnable(Socket socket, int clientNumber){
             this.socket = socket;
             this.clientNumber = clientNumber;
-            username = "User" + Integer.toString(clientNumber);
+            username = "user" + Integer.toString(clientNumber);
             isRunning = true;
+            in = null;
+            out = null;
         }
 
         @Override
         public void run() {
-            BufferedReader in = null;
-            PrintWriter out = null;
 
             try {
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -77,17 +88,38 @@ public class ChattyChatChatServer{
 
                 while(isRunning){
                     String input = in.readLine();
+                    String response = "";
+
                     if ( input != null ) {
-                        out.println("You've entered " + input);
+                        out.println("SERVER: You've entered " + input);
                     }
 
+                    String[] inputArray = input.split(" ", 3);
+                    switch(inputArray[0]){
+                        case "/nick":
+                            changeNick(inputArray);
+                            break;
+                        case "/quit":
+                            quit();
+                            break;
+                        case "/name":
+                            out.println(username);
+                        default:
+                            response = "SERVER: DEFAULT";
+                            message(input);
+                            break;
+                    }
 
-
-                    System.out.println("Sent \"" + input + "\" to client " + clientNumber);
+                    System.out.println("Received \"" + input + "\" from client " + clientNumber + " (" + username + ")");
+                    System.out.println("Sent \"" + response + "\" to client " + clientNumber + " (" + username + ")");
                 }
             }
             catch(IOException e){
                 System.out.println("Error while communicating with client");
+            }
+            catch(Exception e){
+                System.out.println("Unknown Error Occurred");
+                e.printStackTrace();
             }
             finally{
                 try {
@@ -101,15 +133,30 @@ public class ChattyChatChatServer{
 
         }
 
-        private void message(){
-
+        private void message(String input){
+            for( ClientRunnable cr : ChattyChatChatServer.clientRunnables ){
+                if(cr.getIsRunning()){
+                    cr.send(username + ": " + input);
+                }
+            }
         }
-        private void changeNick(){
-
+        private void changeNick(String[] inputArray){
+            if(inputArray.length < 2){
+                send("SERVER: No new username provided");
+            }
+            else {
+                send("SERVER: Username changed from " + username + " to " + inputArray[1]);
+                username = inputArray[1];
+            }
         }
         private void dm(){
 
         }
+
+        public void send( String message ){
+            out.println(message);
+        }
+
         private void quit(){
             isRunning = false;
         }
